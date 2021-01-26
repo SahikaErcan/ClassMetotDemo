c107df3e94a52ec8175e4bfd44dc93209dee4ace
SahikaErcan <68878500+SahikaErcan@users.noreply.github.com>
2021-01-26 14:02:52 +0300

    Başlangıç...

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("~~~ Eklenen Müşteri ~~~~");
+            Console.WriteLine(musteri.Id + "\n" + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("~~~ Silinen Müşteri ~~~~");
+            Console.WriteLine(musteri.Id + "\n" + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,43 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Çetin";
+            musteri1.Yas = 25;
+            musteri1.TCNo = "11111111111";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ayşe";
+            musteri2.Soyad = "Yavuz";
+            musteri2.Yas = 28;
+            musteri2.TCNo = "22222222222";
+
+            MusteriManager musteriManager = new MusteriManager();
+            // Eklenen Müsteri
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+
+            Console.WriteLine("~~~ Müşteri Listesi ~~~");
+            Musteri[] musteri = new Musteri[] {musteri1,musteri2};
+
+            foreach (Musteri m in musteri)
+            {
+                Console.WriteLine("ID No : " + m.Id);
+                Console.WriteLine("Adı : " + m.Ad);
+                Console.WriteLine("Soyadı : " + m.Soyad);
+                Console.WriteLine("Yaş : " + m.Yas);
+                Console.WriteLine("TC No : " + m.TCNo);
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~");
+            }
+
+            // Silinen Müsteri
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
+
+            Console.ReadKey();
         }
     }
 }
