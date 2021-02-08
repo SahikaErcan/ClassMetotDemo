3e1bb8c503a2e239b313370aba6d085fe5e5cc5e
SahikaErcan <68878500+SahikaErcan@users.noreply.github.com>
2021-02-08 17:09:27 +0300

    Dictionary

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryUseIntro
+{
+    class MyDictionary<K,V> 
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKey = keys; // anahtar ve değer verilerini geçici dizilere attık.
+            V[] tempValue = values;
+
+            keys = new K[keys.Length + 1]; // dizi boyutlarını arttırdık
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {// geçici dizilere attığımız değerleri yeni adresimize alıyoruz.
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            // en son eklenen değerleri aldık.
+            keys[keys.Length - 1] = key; 
+            values[values.Length - 1] = value;
+
+        }
+
+        public void Items()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(" " + keys[i] + " nolu müşteri " + values[i]);
+            }
+        }
+       
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DictionaryUseIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customer = new MyDictionary<int, string>();
+            customer.Add(0, "Kemal Sunal");
+            customer.Add(1, "Kadir İnanır");
+            customer.Add(2, "Türkan Şoray");
+            customer.Add(3, "Cüneyt Arkın");
+            customer.Add(4, "Filiz Akın");
+            customer.Add(5, "Şener Şen");         
+            customer.Add(6, "Hülya Koçyiğit");
+            customer.Add(7, "Fatma Girik");
+            
+            Console.WriteLine("~~ Müşteri Bilgileri ~~");
+            customer.Items();
+            
+            Console.WriteLine("Eleman Sayısı : " + customer.Count);
+            Console.ReadKey();
+        }
+    }
+}
